4a487eec2f730ee254a35b58eb65ad199d7a5808
victor Rojas <victor.rojas@ec.pe>
2016-08-05 20:04:15 -0500

    cambio

---
+++ ejemplo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by victor on 05/08/16.
+ */
+var a = 9;
+a += 4;
+function mostrar(b) {
+    console.log(b);
+}
+mostrar('hola');
+//# sourceMappingURL=ejemplo.js.map
---
+++ ejemplo.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by victor on 05/08/16.
+ */
+var a:number = 9;
+a += 4;
+
+function mostrar(b:string) :void{
+    console.log(b);
+}
+mostrar('hola');
